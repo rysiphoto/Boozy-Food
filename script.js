2a7b8de86ab3790f74b5ff8c38b281c3fcd0c6e2
Ryan Siverson <rcsskier@mac.com>
2020-04-09 12:09:04 -0600

    logo uploading

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 
+// on load call the init function which will call the getRecipe function
+// to pre-populate the recipe accordion with pork chop recipes
+window.onload = function () {
+    this.init()
+
+}
+
+function init() {
+    getRecipe('pork chops')
+}
 
 var searchDrink = "";
 
@@ -17,14 +27,32 @@ function createDrinkDiv() {
         var drinkInst = response.drinks[0].strInstructions;
         var drinkImg = response.drinks[0].strDrinkThumb
         var drinkGlass = response.drinks[0].strGlass;
-        var drinkIng1 = response.drinks[0].strIngredient1;
-        var drinkIng2 = response.drinks[0].strIngredient2;
-        var drinkIng3 = response.drinks[0].strIngredient3;
-        var drinkIng4 = response.drinks[0].strIngredient4;
-        var drinkIng5 = response.drinks[0].strIngredient5;
-        var drinkIng6 = response.drinks[0].strIngredient6;
-        var drinkIng7 = response.drinks[0].strIngredient7;
-        var drinkIng8 = response.drinks[0].strIngredient8;
+
+        if (response.drinks[0].strIngredient1 !== null) {
+            var drinkIng1 = response.drinks[0].strIngredient1;
+        }
+        if (response.drinks[0].strIngredient2 !== null) {
+            var drinkIng2 = response.drinks[0].strIngredient2;
+        }
+        if (response.drinks[0].strIngredient3 !== null) {
+            var drinkIng3 = response.drinks[0].strIngredient3;
+        }
+        if (response.drinks[0].strIngredient4 !== null) {
+            var drinkIng4 = response.drinks[0].strIngredient4;
+        }
+        if (response.drinks[0].strIngredient5 !== null) {
+            var drinkIng5 = response.drinks[0].strIngredient5;
+        }
+        if (response.drinks[0].strIngredient6 !== null) {
+            var drinkIng6 = response.drinks[0].strIngredient6;
+        }
+        if (response.drinks[0].strIngredient7 !== null) {
+            var drinkIng7 = response.drinks[0].strIngredient7;
+        }
+        if (response.drinks[0].strIngredient8 !== null) {
+            var drinkIng8 = response.drinks[0].strIngredient8;
+        }
+
 
         var drinkMeasure1 = response.drinks[0].strMeasure1;
         var drinkMeasure2 = response.drinks[0].strMeasure2;
@@ -41,26 +69,32 @@ function createDrinkDiv() {
         var drinkGlassDiv = document.createElement("h3");
         var drinkDis = document.getElementById("drinkDisc");
 
-        // var ingredientNum = 1;
-        // var ingredients = []; // array to push ingredients to once you pull them from the object; an array will be easier to work with in your code
-
-        // while (response.drinks[0]["strIngredient" + ingredientNum]) {
-        //     ingredients.push(response.drinks[0]["strIngredient" + ingredientNum]);
-        //     ingredientNum++;
-
-        // }
-
 
         // Create For Loops for Ingredients if possible
-
-        var drinkIngList1 = document.createElement("li");
-        var drinkIngList2 = document.createElement("li");
-        var drinkIngList3 = document.createElement("li");
-        var drinkIngList4 = document.createElement("li");
-        var drinkIngList5 = document.createElement("li");
-        var drinkIngList6 = document.createElement("li");
-        var drinkIngList7 = document.createElement("li");
-        var drinkIngList8 = document.createElement("li");
+        if (response.drinks[0].strIngredient1 !== null) {
+            var drinkIngList1 = document.createElement("li");
+        }
+        if (response.drinks[0].strIngredient2 !== null) {
+            var drinkIngList2 = document.createElement("li");
+        }
+        if (response.drinks[0].strIngredient3 !== null) {
+            var drinkIngList3 = document.createElement("li");
+        }
+        if (response.drinks[0].strIngredient4 !== null) {
+            var drinkIngList4 = document.createElement("li");
+        }
+        if (response.drinks[0].strIngredient5 !== null) {
+            var drinkIngList5 = document.createElement("li");
+        }
+        if (response.drinks[0].strIngredient6 !== null) {
+            var drinkIngList6 = document.createElement("li");
+        }
+        if (response.drinks[0].strIngredient7 !== null) {
+            var drinkIngList7 = document.createElement("li");
+        }
+        if (response.drinks[0].strIngredient8 !== null) {
+            var drinkIngList8 = document.createElement("li");
+        }
         var drinkInstList = document.createElement("p");
         var drinkImage = document.createElement("p");
         drinkImage.id = "drinkPic";
@@ -71,14 +105,30 @@ function createDrinkDiv() {
         // console.log("final ingredients array:", ingredients);
         drinkNameDiv.textContent = response.drinks[0].strDrink;
         drinkGlassDiv.textContent = ("Glass: " + drinkGlass);
-        drinkIngList1.textContent = (drinkIng1 + ": " + drinkMeasure1);
-        drinkIngList2.textContent = (drinkIng2 + ": " + drinkMeasure2);
-        drinkIngList3.textContent = (drinkIng3 + ": " + drinkMeasure3);
-        drinkIngList4.textContent = (drinkIng4 + ": " + drinkMeasure4);
-        drinkIngList5.textContent = (drinkIng5 + ": " + drinkMeasure5);
-        drinkIngList6.textContent = (drinkIng6 + ": " + drinkMeasure6);
-        drinkIngList7.textContent = (drinkIng7 + ": " + drinkMeasure7);
-        drinkIngList8.textContent = (drinkIng8 + ": " + drinkMeasure8);
+        if (response.drinks[0].strIngredient1 !== null) {
+            drinkIngList1.textContent = (drinkIng1 + ": " + drinkMeasure1);
+        }
+        if (response.drinks[0].strIngredient2 !== null) {
+            drinkIngList2.textContent = (drinkIng2 + ": " + drinkMeasure2);
+        }
+        if (response.drinks[0].strIngredient3 !== null) {
+            drinkIngList3.textContent = (drinkIng3 + ": " + drinkMeasure3);
+        }
+        if (response.drinks[0].strIngredient4 !== null) {
+            drinkIngList4.textContent = (drinkIng4 + ": " + drinkMeasure4);
+        }
+        if (response.drinks[0].strIngredient5 !== null) {
+            drinkIngList5.textContent = (drinkIng5 + ": " + drinkMeasure5);
+        }
+        if (response.drinks[0].strIngredient6 !== null) {
+            drinkIngList6.textContent = (drinkIng6 + ": " + drinkMeasure6);
+        }
+        if (response.drinks[0].strIngredient7 !== null) {
+            drinkIngList7.textContent = (drinkIng7 + ": " + drinkMeasure7);
+        }
+        if (response.drinks[0].strIngredient8 !== null) {
+            drinkIngList8.textContent = (drinkIng8 + ": " + drinkMeasure8);
+        }
         drinkInstList.textContent = drinkInst;
         drinkImage.innerHTML = ("<img src='" + drinkImg + "'>");
 
@@ -89,14 +139,30 @@ function createDrinkDiv() {
 
         currentDiv.appendChild(drinkNameDiv);
         currentDiv.appendChild(drinkGlassDiv);
-        currentDiv.appendChild(drinkIngList1);
-        currentDiv.appendChild(drinkIngList2);
-        currentDiv.appendChild(drinkIngList3);
-        currentDiv.appendChild(drinkIngList4);
-        currentDiv.appendChild(drinkIngList5);
-        currentDiv.appendChild(drinkIngList6);
-        currentDiv.appendChild(drinkIngList7);
-        currentDiv.appendChild(drinkIngList8);
+        if (response.drinks[0].strIngredient1 !== null) {
+            currentDiv.appendChild(drinkIngList1);
+        }
+        if (response.drinks[0].strIngredient2 !== null) {
+            currentDiv.appendChild(drinkIngList2);
+        }
+        if (response.drinks[0].strIngredient3 !== null) {
+            currentDiv.appendChild(drinkIngList3);
+        }
+        if (response.drinks[0].strIngredient4 !== null) {
+            currentDiv.appendChild(drinkIngList4);
+        }
+        if (response.drinks[0].strIngredient5 !== null) {
+            currentDiv.appendChild(drinkIngList5);
+        }
+        if (response.drinks[0].strIngredient6 !== null) {
+            currentDiv.appendChild(drinkIngList6);
+        }
+        if (response.drinks[0].strIngredient7 !== null) {
+            currentDiv.appendChild(drinkIngList7);
+        }
+        if (response.drinks[0].strIngredient8 !== null) {
+            currentDiv.appendChild(drinkIngList8);
+        }
         drinkDis.appendChild(drinkInstList);
         drinkPicDiv.appendChild(drinkImage);
 
@@ -110,14 +176,14 @@ $(".recipe-btn").on("click", function () {
 
     // var returnResultsNum = $("#exampleFormControlSelect1"); //on pause
     var userSearchTerm = $("#searchBar").val();
+    getRecipe(userSearchTerm)
+});
 
-    var rapidKey = '6231706c36msh4d43c424ff96c50p195ed0jsn6762f2140659'
-    var queryUrl = `https://api.spoonacular.com/food/products/search?query=${userSearchTerm}&apiKey=${rapidKey}`
-    $('#firstContent').empty()
-    $('#secondContent').empty()
-    $('#thirdContent').empty()
-    $('#fourthContent').empty()
-    $('#fithContent').empty()
+function getRecipe(searchTerm) {
+    var rapidKey = '6231706c36msh0aa2e9f7a2ee4f409cae6e1dc3cdad9c4d43c424ff96c50p195ed0jsn6762f2140659'
+    var queryUrl = `https://api.spoonacular.com/food/products/search?query=${searchTerm}&apiKey=${rapidKey}`
+    $('#accordion').empty()
+
     var settings = {
         "async": true,
         "crossDomain": true,
@@ -132,7 +198,6 @@ $(".recipe-btn").on("click", function () {
 
         for (var i = 0; i < 5; i++) {
             var recipeID = response[i].id
-            console.log(recipeID)
             var newSettings = {
                 "async": true,
                 "crossDomain": true,
@@ -148,43 +213,26 @@ $(".recipe-btn").on("click", function () {
             }
             //second call
             $.ajax(newSettings).then(function (results) {
-                console.log(results)
+
 
                 var h3 = $(`<h3>${results.title}</h3>`)
+                var content = $('<div>')
+                content.append(`<img src="${results.image}" alt="${results.title}">`)
+                var content = $(`<div><img src="${results.image}" alt="${results.title}"><p><strong>STEPS:</strong></p></div>`)
+                for (var i = 0; i < results.analyzedInstructions[0].steps.length; i++) {
 
-                var content = $(`<div><img src="${results.image}" alt="${results.title}"></div>`)
+                    content.append(`<p><strong>${results.analyzedInstructions[0].steps[i].number}</strong> ${results.analyzedInstructions[0].steps[i].step}`)
+                }
                 $("#accordion").append(h3, content)
                 $("#accordion").accordion("refresh");
 
-                // switch(i) {
-                // 	case 0:
-                // 		$( "#firstHeader" ).text(results.title)
-                // 		$("#firstContent").append(`<p><img src="${results.image}" alt="${results.title}"></p>`)
-                // 	  break;
-                // 	case 1:
-                // 		$( "#secondHeader" ).text(results.title)
-                // 		$("#secondContent").append(`<p><img src="${results.image}" alt="${results.title}"></p>`)
-                // 	  break;
-                // 	case 2:
-                // 		$( "#thirdHeader" ).text(results.title)
-                // 		$("#thirdContent").append(`<p><img src="${results.image}" alt="${results.title}"></p>`)
-                // 	  break;
-                // 	case 3:
-                // 		$( "#forthHeader" ).text(results.title)
-                // 		$("#forthContent").append(`<p><img src="${results.image}" alt="${results.title}"></p>`)	
-                // 	break;
-                // 	default:
-                // 		$( "#fithHeader" ).text(results.title)
-                // 		$("#fithContent").append(`<p><img src="${results.image}" alt="${results.title}"></p>`)
-                // }
+
 
             })
 
         }
-        //document.getElementById("recipeHere").innerHTML = response[0].title;
     });
 
 
 });
 $("#accordion").accordion();
-
